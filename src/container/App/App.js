02b5b75bcd4cf32c721b75e91964bc8c14704f7a
Диденко Егор <didenko_e@tass.ru>
2019-04-11 18:42:39 +0300

    routs for sigin and signup pages

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,30 +1,58 @@
 import React      from 'react';
 import SignIn       from '../Auth/SignIn';
 import SignUp       from '../Auth/SignUp';
-// import Nav         from '../Navigation/Navigations'
 import AuthExample  from '../Navigation/Auth'
-import './App.scss';
 import PublickPage from '../PublickPage/PublickPage'
-import {BrowserRouter as Router, Route, Link, Redirect, withRouter} from "react-router-dom";
+import {BrowserRouter as Router, Route, Redirect, withRouter, Switch} from "react-router-dom";
+import './App.scss';
 
+class Home extends React.PureComponent {
+    previousLocation = this.props.location;
 
+    componentDidMount() {
+        if( this.props.location.state && this.props.location.state.modal) {
+            this.props.history.push({pathname: "/", state:{modal: false}});
+        }
+    }
 
+    componentWillUpdate(nextProps) {
+        let { location } = this.props;
+        if (
+            nextProps.history.action !== "POP" &&
+            (!location.state || !location.state.modal)
+        ) {
+            this.previousLocation = this.props.location;
+        }
+    }
 
-class App extends React.Component {
-  render() {
-    return (
-        <Router>
-          <Route exact path="/" component={PublickPage} />
-          <Route path="/sign-in" component={SignIn} />
-          <Route path="/sign-up" component={SignUp} />
-            <Link to="/" >Home</Link>
-          <AuthExample/>
-        </Router>
-    );
-  }
+    render() {
+        let { location } = this.props;
+        let isModal = (location.state && location.state.modal && this.previousLocation !== location);
+
+        return (
+            <>
+                <Switch location={isModal ? this.previousLocation : location} >
+                    <Route exact path="/" component={PublickPage} />
+                </Switch>
+                {isModal ? <Route path="/sign-in" component={SignIn} /> : null}
+                {isModal ? <Route path="/sign-up" component={SignUp} /> : null}
+            </>
+        )
+    }
+
 }
 
-export default App;
+export default function App() {
+    return (
+        <Router>
+            <Route  component={Home}/>
+        </Router>
+    );
+}
+
+//
+// <Link to="/" >Home</Link>
+// <AuthExample/>
 //
 // import TestStand  from '../TestEnvironment/TestStand'
 // import Header     from '../../component/Header/Header';
